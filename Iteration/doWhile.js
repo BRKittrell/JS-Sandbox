aa0ff1d90f9231539d97c8c34871e7ccee908957
Brandon Kittrell <brandonkittrell@Brandons-MBP.lan1>
2022-12-06 13:06:00 -0500

    added content

---
+++ Iteration/doWhile.js
@@ -0,0 +1,9 @@
+function doThis (num){
+    
+var i = num;
+    do {
+  i += 3;
+  console.log(i);
+} while (i < 40);
+}
+doThis(30)
---
+++ Iteration/stringIteration.js
@@ -0,0 +1,9 @@
+function stringLooping (str){
+let strLoop 
+
+for (var i = 0; i < str.length; i++){
+    console.log(str[i])
+}
+}
+
+stringLooping("I am a string.")
