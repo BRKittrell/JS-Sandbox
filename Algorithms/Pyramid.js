825490521f57977012fbfc521e2cb3edfa9e1207
Brandon Kittrell <brandonkittrell@Brandons-MacBook-Pro.local>
2022-12-09 13:40:24 -0500

    added algo

---
+++ Algorithms/Pyramid.js
@@ -0,0 +1,24 @@
+function pyramid(num) {
+    const arr = []
+
+    // other function
+    let hash = '#'
+
+    // loop to the number
+    for(let i = 1; i <= num; i++) {
+        arr.push(hash)
+        hash = hash + '#'
+    }
+    return arr
+
+}
+
+function buildPyrmaid(arr) {
+    arr.forEach((elem) => {
+        console.log(elem)
+    })
+}
+
+
+
+buildPyrmaid(pyramid(10))
